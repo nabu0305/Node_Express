ef70b9cb47ff44594bf167ca092d98db75a52ea1
nabu0305 <nabu010305@gmail.com>
2022-04-09 06:34:23 +0000

    description express

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
 // app.get('/', (req, res) => {res.send('Hello World!')})
 
 app.get('/',function(request,response){
@@ -20,9 +22,32 @@ app.get('/',function(request,response){
         });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get('/page/:pageId',function(request,response){
+	fs.readdir('./data', function(error, filelist){
+          var filteredId = path.parse(request.params.pageId).base;
+          fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+            var title = request.params.pageId;
+            var sanitizedTitle = sanitizeHtml(title);
+            var sanitizedDescription = sanitizeHtml(description, {
+              allowedTags:['h1']
+            });
+            var list = template.list(filelist);
+            var html = template.HTML(sanitizedTitle, list,
+              `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+              ` <a href="/create">create</a>
+                <a href="/update?id=${sanitizedTitle}">update</a>
+                <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${sanitizedTitle}">
+                  <input type="submit" value="delete">
+                </form>`
+            );
+            response.send(html);
+          });
+        });
+});
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
 /*var http = require('http');
 var fs = require('fs');
 var url = require('url');
@@ -32,22 +57,7 @@ var path = require('path');
 var sanitizeHtml = require('sanitize-html');
 
 var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    if(pathname === '/'){
-      if(queryData.id === undefined){
-        fs.readdir('./data', function(error, filelist){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = template.list(filelist);
-          var html = template.HTML(title, list,
-            `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`
-          );
-          response.writeHead(200);
-          response.end(html);
-        });
+    
       } else {
         fs.readdir('./data', function(error, filelist){
           var filteredId = path.parse(queryData.id).base;
