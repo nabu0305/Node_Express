bd0f761602ae53a96e3b3a9605bda4f86ec8f509
nabu0305 <nabu010305@gmail.com>
2022-04-11 10:44:28 +0000

    mongoose connect

--- main.js
+++ main.js
@@ -7,9 +7,21 @@ var bodyParser = require('body-parser');
 var sanitizeHtml = require('sanitize-html');
 var compression = require('compression')
 var template = require('./lib/template.js');
-const helmet = require('helmet')
-app.use(helmet());
+const helmet = require('helmet');
+const db = require('./db.js');
+const mongoose = require("mongoose");
 
+const mdb = mongoose.connection;
+db();
+Client.connect('mongodb://localhost:3000/school', function(error, db){
+    if(error) {
+        console.log(error);
+    } else {
+        console.log("connected:"+db);
+        db.close();
+    }
+});
+app.use(helmet());	
 var topicRouter = require('./routes/topic.js');
 var indexRouter = require('./routes/index.js');
 app.use(express.static('public'));
@@ -25,7 +37,12 @@ app.get('*', function(request, response, next){
 app.use('/topic', topicRouter);
 app.use('/', indexRouter);
 
+var pages = mongoose.Schema({
+    title : 'string',
+    description : 'string',
+});
 
+var Pages = mongoose.model('Schema', pages);
 
 app.use(function(req, res, next) {
   res.status(404).send('Sorry cant find that!');
